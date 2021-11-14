88143a9924014231b7c2ca9ea5413a673768dc93
Pier-Luc Brault <pier-luc@brault.me>
2021-11-14 13:47:03 -0500

    Change CSS location

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 
-import '../src/style.css';
+import '../css/main.css';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
