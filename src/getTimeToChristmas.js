c1844dfbcc53bc3645759d51460689d4ea7c58f1
Pier-Luc Brault <pier-luc@brault.me>
2021-11-14 15:36:43 -0500

    Handle case where Christmas is passed

--- src/getTimeToChristmas.js
+++ src/getTimeToChristmas.js
@@ -3,7 +3,13 @@ import { DateTime, Interval } from 'luxon';
 function getTimeToChristmas() {
   const now = DateTime.now();
   const currentYear = now.year;
-  const christmas = DateTime.local(currentYear, 12, 25, 0, 0, 0);
+
+  let christmas;
+  if (now.month === 12 && now.day >= 25) {
+    christmas = DateTime.local(currentYear + 1, 12, 25, 0, 0, 0);
+  } else {
+    christmas = DateTime.local(currentYear, 12, 25, 0, 0, 0);
+  }
 
   const interval = Interval.fromDateTimes(now, christmas);
   const duration = interval.toDuration(['days', 'hours', 'minutes', 'seconds']).toObject();
