d65fcd4cd07305eefd29c2ab91b69f1561cf8b15
Pier-Luc Brault <pier-luc@brault.me>
2021-11-14 14:27:49 -0500

    Add snow and colors

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+
 import Box from '@mui/material/Box';
+import Snowfall from 'react-snowfall';
 import Stack from '@mui/material/Stack';
 
 import getTimeToChristmas from '../src/getTimeToChristmas';
@@ -16,32 +18,35 @@ export default function Index() {
   const { days, hours, minutes, seconds } = timeToChristmas;
 
   return (
-    <Stack
-      className="main-container"
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      spacing={2}
-    >
-      <Box className="christmas-trees">
-        🎄🎄🎄
-      </Box>      
-      <Box className="label">
-        Time Left Before Christmas
-      </Box>
-      <Box className="time-before-christmas">
-        <span className="number">{String(days).padStart(2, '0')}</span>
-        &nbsp;days&nbsp;
-        <span className="number">{String(hours).padStart(2, '0')}</span>
-        &nbsp;hours&nbsp;
-        <span className="number">{String(minutes).padStart(2, '0')}</span>
-        &nbsp;minutes&nbsp;
-        <span className="number">{String(seconds).padStart(2, '0')}</span>
-        &nbsp;seconds&nbsp;
-      </Box>
-      <Box className="christmas-trees">
-        🎄🎄🎄
-      </Box>
-    </Stack>
+    <>
+      <Snowfall />
+      <Stack
+        className="main-container"
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <Box className="label">
+          Time Left Before Christmas
+        </Box>
+        <Box className="christmas-trees">
+          🎄🎄🎄
+        </Box>    
+        <Box className="time-before-christmas">
+          <span className="number">{String(days).padStart(2, '0')}</span>
+          &nbsp;days&nbsp;
+          <span className="number">{String(hours).padStart(2, '0')}</span>
+          &nbsp;hours&nbsp;
+          <span className="number">{String(minutes).padStart(2, '0')}</span>
+          &nbsp;minutes&nbsp;
+          <span className="number">{String(seconds).padStart(2, '0')}</span>
+          &nbsp;seconds&nbsp;
+        </Box>
+        <Box className="christmas-trees">
+          🎄🎄🎄
+        </Box>
+      </Stack>
+    </>
   );
 }
