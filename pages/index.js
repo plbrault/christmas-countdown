8b5f669a877227e2fc053540f04358826e38f950
Pier-Luc Brault <pier-luc@brault.me>
2022-10-14 15:20:46 -0400

    Allow to hide footer

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import dynamic from 'next/dynamic';
 
@@ -7,13 +7,20 @@ import Countdown from '../src/Countdown';
 const Snowfall = dynamic({ loader: () => import('react-snowfall') },{ loading: () => <div></div>, ssr: false }) // eslint-disable-line
 
 export default function Index() {
+  const [showFooter, setShowFotter] = useState(true);
+
   return (
     <>
       <Snowfall />
       <Countdown />
-      <footer>
-        <a href="https://plbrault.com">&copy; 2022 P-L.Brault</a>
-      </footer>
+      {
+        showFooter ? (
+          <footer>
+            <a href="https://plbrault.com">&copy; 2022 P-L.Brault</a>
+            <button type="button" className="hide-footer" onClick={() => setShowFotter(false)}>hide</button>
+          </footer>
+        ) : ''
+      }
     </>
   );
 }
