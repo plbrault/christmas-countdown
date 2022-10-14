2d27744b6cc3f668522f59ab465b33b8ab2b746c
Pier-Luc Brault <pier-luc@brault.me>
2022-10-14 15:29:12 -0400

    Add hideable copyright notice (#8)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import dynamic from 'next/dynamic';
 
@@ -7,10 +7,31 @@ import Countdown from '../src/Countdown';
 const Snowfall = dynamic({ loader: () => import('react-snowfall') },{ loading: () => <div></div>, ssr: false }) // eslint-disable-line
 
 export default function Index() {
+  const [year, setYear] = useState((new Date().getFullYear()));
+  const [showFooter, setShowFotter] = useState(true);
+
+  useEffect(() => {
+    setYear((new Date().getFullYear()));
+  });
+
   return (
     <>
       <Snowfall />
       <Countdown />
+      {
+        showFooter ? (
+          <footer>
+            <a href="https://plbrault.com">
+              &copy;
+              &nbsp;
+              {year}
+              &nbsp;
+              P-L.Brault
+            </a>
+            <button type="button" className="hide-footer" onClick={() => setShowFotter(false)}>hide</button>
+          </footer>
+        ) : ''
+      }
     </>
   );
 }
