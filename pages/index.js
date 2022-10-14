372990a8b5996bea709f48d5c37c3ba4e5a80c69
Pier-Luc Brault <pier-luc@brault.me>
2022-10-14 15:28:44 -0400

    Make copyright year dynamic

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import dynamic from 'next/dynamic';
 
@@ -7,7 +7,12 @@ import Countdown from '../src/Countdown';
 const Snowfall = dynamic({ loader: () => import('react-snowfall') },{ loading: () => <div></div>, ssr: false }) // eslint-disable-line
 
 export default function Index() {
+  const [year, setYear] = useState((new Date().getFullYear()));
   const [showFooter, setShowFotter] = useState(true);
+
+  useEffect(() => {
+    setYear((new Date().getFullYear()));
+  });
 
   return (
     <>
@@ -16,7 +21,13 @@ export default function Index() {
       {
         showFooter ? (
           <footer>
-            <a href="https://plbrault.com">&copy; 2022 P-L.Brault</a>
+            <a href="https://plbrault.com">
+              &copy;
+              &nbsp;
+              {year}
+              &nbsp;
+              P-L.Brault
+            </a>
             <button type="button" className="hide-footer" onClick={() => setShowFotter(false)}>hide</button>
           </footer>
         ) : ''
