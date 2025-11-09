dcae323ad24e44c48d3173e04010841be706c5f5
Pier-Luc Brault <pier-luc@brault.me>
2025-11-09 12:07:47 -0500

    Replace Microanalytics with Pirsch (#12)

--- pages/_app.js
+++ pages/_app.js
@@ -37,7 +37,7 @@ export default function MyApp(props) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="og:image" content="https://yetanotherchristmascountdown.com/social-media-picture.jpg" />
 
-        <script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer>{}</script>
+        <script defer src="https://api.pirsch.io/pa.js" id="pianjs" data-code="K9sNNFaeBvFqMPS3VZNc7q8rUC3rWRwb"></script>
 
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" href="/favicon-32x32.png" />
